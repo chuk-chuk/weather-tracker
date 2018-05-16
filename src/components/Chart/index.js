2068f51760da6e33fdce5d289e5f1059578be5a6
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-05-16 13:56:11 +0100

    use react-charts-js to render static weather data using component state - will be replaced with the api dynamic data

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,40 +1,81 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Line } from 'react-chartjs-2';
 import { fetchItems } from '../../reducers/thunk/thunkAction';
 import moment from 'moment';
 import './styles.css';
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 
-
 class Chart extends Component {
-
-    componentDidMount() {
-        this.props.fetchItems(`http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=${API_KEY}`);
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData: {
+        labels: ['Monday','Tuesday','Wednesday','Thursday','Friday'],
+        datasets: [
+          {
+            label: 'weather variations during 5 days',
+            data: [
+              21,16,14,18,15, 0
+            ],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.3)'
+            ]
+          }
+        ]
+      }
     }
+  }
 
-    render(){
-        if (this.props.hasErrored) {
-            return <p>Sorry! There was an error loading the items</p>;
-        }
-        if(this.props.isLoading) {
-            return <p>Loading…</p>;
-        }
-        console.log('payload', this.props);
-        console.log(this.state);
+  componentDidMount() {
+      this.props.fetchItems(`http://api.openweathermap.org/data/2.5/forecast?q=London&units=metric&appid=${API_KEY}`);
+  }
 
-        return (
-          <div>
-            <ul>
-                {this.props.items.map((item) => (
-                    <li key={item.dt}>
-                        {moment(item.dt_txt).format('dddd HH:mm')} Temperature {Math.round(item.main.temp)}
-                    </li>
-                ))}
-            </ul>
-        </div>
-      );
-    }
+  static defaultProps = {
+    displayTitle: true,
+    displayLegend: true,
+    legendPosition: 'bottom'
+  }
+
+  render(){
+      if (this.props.hasErrored) {
+          return <p>Sorry! There was an error loading the items</p>;
+      }
+      if(this.props.isLoading) {
+          return <p>Loading…</p>;
+      }
+      console.log('payload', this.props);
+      console.log(this.state);
+
+      return (
+        <div>
+          <ul>
+              {this.props.items.map((item) => (
+                  <li key={item.dt}>
+                      {moment(item.dt_txt).format('dddd HH:mm')} Temperature {Math.round(item.main.temp)}
+                  </li>
+              ))}
+          </ul>
+          <Line
+            data={this.state.chartData}
+            width={50}
+            height={25}
+            options={{
+              title: {
+                display: this.props.displayTitle,
+                text: 'Weather forecast for the last 5 days in London',
+                fontSize: 25
+              },
+              legend: {
+                display: this.props.displayLegend,
+                position: this.props.legendPosition
+              }
+            }}
+          />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
