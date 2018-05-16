5fa373cd5d643839f6f69b74c71adb091c98bac1
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-05-16 17:41:08 +0100

    add display options to the chart

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 
 # Currency Tracker
 
-An application that tracks the currency trends for the given period. The application uses visual charts and custom date picker. 
+An application that displays five days weather data for the chosen location as well as pointing to the same location on the Google Map. The application uses visualisation charts using react-chars.js and google API to render map.
+As a user you can search a city in the search bar component or alternatively drag the map and pick a city from any location by passing coordinates with a request url to OpenWeatherApi.
 
 ## How to get started
 - Pull the repository
--- src/components/BarGraph/index.js
+++ src/components/BarGraph/index.js
@@ -3,7 +3,7 @@ import { Line } from 'react-chartjs-2';
 import moment from 'moment';
 
 const BarGraph = (props) => {
-  console.log(props);
+  console.log('barGraph props', props);
   const rangeOfDays = props.weatherData.map((item) => moment(item.dt_txt).format('dddd HH:mm'));
   const temperature = props.weatherData.map((item) => Math.round(item.main.temp));
 
@@ -42,9 +42,27 @@ const BarGraph = (props) => {
             top: 20
           }
         },
-        tooltips: {
-          enabled: false
+        scales: {
+          xAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'labels'
+            }
+        }],
+        yAxes: [{
+        ticks: {
+            max: 30,
+            stepValue: 5,
+            steps: 10,
+            beginAtZero: true
+          }
+        }]
         },
+        hover: {
+          mode: 'label'
+        },
+        showAllTooltips: true,
         legend: {
           display: true,
           position: 'bottom',
