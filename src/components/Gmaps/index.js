0b6792b8ca6f9d4cbb54cf4a374657fae72b338a
Yulia Litvinchuk <yulia.v.litvin@gmail.com>
2018-05-21 16:49:44 +0100

    intergate google map react package

--- src/components/Gmaps/index.js
+++ src/components/Gmaps/index.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './styles.css';
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+const AnyReactComp = ({ text }) => <div>{ text }</div>
 
 export default class Gmaps extends Component {
-
-    render() {
-        return (
-            <div>Google Maps</div>
-        );
-    }
+  static defaultProps = {
+    center: { lat: 51.51, lng: 0.13 },
+    zoom: 8
+  }
+  render() {
+    return (
+      <div style={{ height: '50vh', width: '50%', paddingLeft: '90px' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{
+            key: `${API_KEY}`,
+          }}
+          defaultCentre={this.props.center}
+          center={this.props.center}
+          defaultZoom={this.props.zoom}>
+          <AnyReactComp
+            lat={ 51.51 }
+            lng={ 0.14 }
+            text={`I am on the map`}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
 }
